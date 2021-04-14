543e9f0ee6d77f7464cb3fedd003d043f90fce76
ims43 <ims43@tamu.edu>
2021-04-14 15:55:44 -0500

    Flask project structure

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def homepage():
+    return render_template("index.html")
+
+
+@app.route('/thanks')
+def special_thanks():
+    return render_template("special_thanks.html")
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+
+if __name__ == '__main__':
+    app.run()
