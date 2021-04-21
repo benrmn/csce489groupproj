60f47906e7f1c72e8cc44f892bf1bd53f2917f01
ims43 <ims43@tamu.edu>
2021-04-21 16:48:42 -0500

    Parse csv and get images

--- app.py
+++ app.py
@@ -1,39 +1,61 @@
 from flask import Flask, render_template, request
 import requests
 import json
+import csv
 
 app = Flask(__name__)
 
-headers = {
-    'User-Agent': 'csce 489 ggs project',
-    'From': 'bramon24@tamu.edu',
-}
 
-games = ['RIFT', 'Rust', 'Quake Champions', 'Pummel Party', 'Fall Guys: Ultimate Knockout']
+def get_images(games):
+    headers = {
+        'User-Agent': 'csce 489 ggs project',
+        'From': 'bramon24@tamu.edu',
+    }
 
-dictgame = {}
-for game in games:
-    payload = {'search': game}
+    games = ['RIFT', 'Rust', 'Quake Champions', 'Pummel Party', 'Fall Guys: Ultimate Knockout']
 
-    url = "https://api.rawg.io/api/games?key=c3f3802ae5a145e68a6bc2a687e8f25f"
+    dictgame = {}
+    for game in games:
+        payload = {'search': game}
 
-    r = requests.get(url, headers=headers, params=payload)
+        url = "https://api.rawg.io/api/games?key=c3f3802ae5a145e68a6bc2a687e8f25f"
 
-    data = json.loads(r.text)
-    for gamenm in data['results']:
-        if game == gamenm['name']:
-            # add logic here for if api cannot return any image
-            # was thinking we use our logo (in github already)
-            #if gamenm['background_image'] == None:
-            #    dictgame[gamenm['name']] = 'ggslogo.png'
-            #else:
-            #    dictgame[gamenm['name']] = gamenm['background_image']
-                
-            dictgame[gamenm['name']] = gamenm['background_image']
+        r = requests.get(url, headers=headers, params=payload)
 
-games_names = list(dictgame)
-vals = dictgame.values()
-games_images = list(vals)
+        data = json.loads(r.text)
+        for gamenm in data['results']:
+            if game == gamenm['name']:
+                # add logic here for if api cannot return any image
+                # was thinking we use our logo (in github already)
+                if gamenm['background_image'] == None:
+                   dictgame[gamenm['name']] = 'ggslogo.png'
+                else:
+                   dictgame[gamenm['name']] = gamenm['background_image']
+
+                dictgame[gamenm['name']] = gamenm['background_image']
+
+    games_names = list(dictgame)
+    vals = dictgame.values()
+    games_images = list(vals)
+
+    return games_names, games_images
+
+
+def get_user_recs(filename, username):
+    top_5_recs = []
+    with open(filename) as file:
+        csv_reader = csv.reader(file, delimiter=',')
+        for row in csv_reader:
+            if row[0] == username:
+                for i in range(1, 6):
+                    top_5_recs.append(row[i])
+            break
+    g_name, g_image = get_images(top_5_recs)
+    return g_name, g_image
+
+
+# default games
+games_names, games_images = get_images(['RIFT', 'Rust', 'Quake Champions', 'Pummel Party', 'Fall Guys: Ultimate Knockout'])
 
 
 @app.route('/')
@@ -44,7 +66,8 @@ def homepage():
 @app.route('/recs', methods=['POST'])
 def recs():
     username = request.form['username']
-    return render_template("index.html", dgames=games_images, ngames=games_names)
+    g_names, g_images = get_user_recs('recommendations.csv', username)
+    return render_template("index.html", dgames=g_images, ngames=g_names)
 
 
 @app.route('/thanks')
