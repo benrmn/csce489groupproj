36506282ec15bae62552fb51d1497d21ed241752
benrmn <70165218+benrmn@users.noreply.github.com>
2021-04-25 18:31:26 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ import urllib3
 
 app = Flask(__name__)
 
-
 def get_images(games):
     headers = {
         'User-Agent': 'csce 489 ggs project',
@@ -87,50 +86,118 @@ def get_links(games):
         if type(games_links[i]) == float:
             games_links[i] = link_start
 
-    print(games_links)
     return games_links
 
 
-def get_user_recs(filename, username):
+def get_user_recs(filename, username, num):
     top_5_recs = []
     with open(filename) as file:
         csv_reader = csv.reader(file, delimiter=',')
         for row in csv_reader:
             if row[0] == username:
-                for i in range(1, 6):
-                    print(row[i])
+                for i in range(1, num+1):
                     top_5_recs.append(row[i])
                 break
         g_name, g_image = get_images(top_5_recs)
         return g_name, g_image
 
 
-def get_most_popular():
+def get_most_popular(num):
     with open('recommendations.csv') as file:
         csv_reader = csv.reader(file, delimiter=',')
         top_popular = []
         for row in csv_reader:
-            for i in range(1, 6):
+            for i in range(1, num+1):
                 top_popular.append(row[i])
             return get_images(top_popular)
 
 
 @app.route('/')
 def homepage():
-    games_names, games_images = get_most_popular()
+    games_names, games_images = get_most_popular(5)
     temp_names = games_names
     games_links = get_links(games_names)
-    return render_template("index.html", dgames=games_images, ngames=temp_names, lgames=games_links)
+    return render_template("indexp.html", dgames=games_images, ngames=temp_names, lgames=games_links)
 
+@app.route('/10pop', methods=['POST'])
+def homepage10():
+    games_names, games_images = get_most_popular(10)
+    temp_names = games_names
+    games_links = get_links(games_names)
+    return render_template("index10p.html", dgames=games_images, ngames=temp_names, lgames=games_links)
+
+@app.route('/15pop', methods=['POST'])
+def homepage15():
+    games_names, games_images = get_most_popular(15)
+    temp_names = games_names
+    games_links = get_links(games_names)
+    return render_template("index15p.html", dgames=games_images, ngames=temp_names, lgames=games_links)
+
+@app.route('/20pop', methods=['POST'])
+def homepage20():
+    games_names, games_images = get_most_popular(20)
+    temp_names = games_names
+    games_links = get_links(games_names)
+    return render_template("index20p.html", dgames=games_images, ngames=temp_names, lgames=games_links)
+
+@app.route('/25pop', methods=['POST'])
+def homepage25():
+    games_names, games_images = get_most_popular(25)
+    temp_names = games_names
+    games_links = get_links(games_names)
+    return render_template("index25p.html", dgames=games_images, ngames=temp_names, lgames=games_links)
 
 @app.route('/recs', methods=['POST'])
 def recs():
     username = request.form['username']
-    g_names, g_images = get_user_recs('recommendations.csv', username)
+    f = open("username.txt", "w")
+    f.truncate(0)
+    f.write(username)
+    f.close()
+    g_names, g_images = get_user_recs('recommendations.csv', username, 5)
     temp_names = g_names
     games_links = get_links(g_names)
-    return render_template("index.html", dgames=g_images, ngames=temp_names, lgames=games_links)
+    return render_template("index.html", dgames=g_images, ngames=temp_names, lgames=games_links, username=username)
 
+@app.route('/10recs', methods=['POST'])
+def recs10():
+    f = open("username.txt", "r")
+    usr = f.readline()
+    f.close()
+    g_names, g_images = get_user_recs('recommendations.csv', usr, 10)
+    temp_names = g_names
+    games_links = get_links(g_names)
+    return render_template("index10.html", dgames=g_images, ngames=temp_names, lgames=games_links, username=usr)
+
+@app.route('/15recs', methods=['POST'])
+def recs15():
+    f = open("username.txt", "r")
+    usr = f.readline()
+    f.close()
+    g_names, g_images = get_user_recs('recommendations.csv', usr, 15)
+    temp_names = g_names
+    games_links = get_links(g_names)
+    return render_template("index15.html", dgames=g_images, ngames=temp_names, lgames=games_links, username=usr)
+
+@app.route('/20recs', methods=['POST'])
+def recs20():
+    f = open("username.txt", "r")
+    usr = f.readline()
+    f.close()
+    g_names, g_images = get_user_recs('recommendations.csv', usr, 20)
+    temp_names = g_names
+    games_links = get_links(g_names)
+    return render_template("index20.html", dgames=g_images, ngames=temp_names, lgames=games_links, username=usr)
+
+@app.route('/25recs', methods=['POST'])
+def recs25():
+    f = open("username.txt", "r")
+    usr = f.readline()
+    f.close()
+    g_names, g_images = get_user_recs('recommendations.csv', usr, 25)
+    temp_names = g_names
+    games_links = get_links(g_names)
+    return render_template("index25.html", dgames=g_images, ngames=temp_names, lgames=games_links, username=usr)
 
 @app.route('/thanks')
 def special_thanks():
